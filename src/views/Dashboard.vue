ce76840812b1e99bcb2ad8183d8f9c5c42b63b48
Arhan Islam <arhanislam3407@gmail.com>
2023-09-30 21:07:47 +0600

    vue auth assignment

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+
+</script>
+
+<template>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row g-3">
+                <div class="card mt-5">
+                    <div class="card-body">
+                        
+                        <h1>This is dashboard</h1>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<style scoped>
+
+.container-fluid{
+    margin: 0;
+    padding: 0;
+    background-image: url('https://getwallpapers.com/wallpaper/full/6/1/8/8539.jpg');
+    background-size: cover;
+    background-repeat: no-repeat;
+    padding-top: 5%;
+   
+    height: 100vh;
+}
+
+.card{
+
+color: aliceblue;
+background-color: rgba(0,0,0,0.5) !important;
+}
+
+</style>
